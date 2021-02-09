fe35855ade2f8d61a3a50847fdb45a7f447ed1f9
JanosGemes <78816649+JanosGemes@users.noreply.github.com>
2021-02-09 19:13:21 +0100

    harmadik

--- Program.cs
+++ Program.cs
@@ -2,7 +2,21 @@
 
 namespace SzabadulasASzobabol
 {
-    class Program
+    public class Targy
+    {
+        int sorszam;
+        string nev, hely;
+        bool aktivalva;
+
+        public Targy(int sorszam, string nev, string hely, bool aktivalva)
+        {
+            this.sorszam = sorszam;
+            this.nev = nev;
+            this.hely = hely;
+            this.aktivalva = aktivalva;
+        }
+    }
+        class Program
     {
         static void Main(string[] args)
         {
