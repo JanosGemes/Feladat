58120d5203aa41f24d9662764d398534e0ae79e4
JanosGemes <78816649+JanosGemes@users.noreply.github.com>
2021-02-09 19:29:19 +0100

    negyedik

--- Program.cs
+++ Program.cs
@@ -15,12 +15,49 @@ namespace SzabadulasASzobabol
             this.hely = hely;
             this.aktivalva = aktivalva;
         }
+
+        public bool Aktive()
+        {
+            return this.aktivalva;
+        
+        }
+
+        public string MelyikSzoba()
+        {
+            return this.hely;
+        }
+
+        public string TargyNeve()
+        {
+            return this.nev;
+        }
+
+        public string Felvetel()
+        {
+            hely = "taska";
+            return hely;
+        
+        }
+
+        
+
     }
         class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!");
+            Targy[] targy = new Targy[]
+                {
+                new Targy (0, "szekrény", "szoba", false)
+                new Targy (1, "ajtó", "szoba", false)
+                new Targy (2, "ablak", "szoba", false)
+                new Targy (3, "doboz", "szoba", false)
+                new Targy (4, "kulcs", "szoba", false)
+                new Targy (5, "feszítővas", "fürdőszoba", false)
+                new Targy (6, "kád", "fürdőszoba", false)
+                new Targy (7, "ágy", "szoba", false)
+
+                };
         }
     }
 }
