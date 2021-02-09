9a1c0169ce915a09e240e4886c471816f1e1987b
JanosGemes <78816649+JanosGemes@users.noreply.github.com>
2021-02-09 20:07:59 +0100

    hetedik
    helyseg osztály + elemei, köszönés, help

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ namespace SzabadulasASzobabol
         public string LetetelSzoba()
         {
             hely = "szoba";
-            return hely;     
+            return hely;
         }
 
         public string LetetelFurdoszoba()
@@ -59,7 +59,6 @@ namespace SzabadulasASzobabol
         {
             hely = "elhúzva";
             return hely;
-
         }
 
         public void NezdTargy()
@@ -96,22 +95,108 @@ namespace SzabadulasASzobabol
         }
 
     }
-        class Program
+
+    public class Helyseg
     {
+        bool jelenlet;
+        string szobaNeve;
+
+        public Helyseg(string szobaNeve, bool jelenlet)
+        {
+            this.szobaNeve = szobaNeve;
+            this.jelenlet = jelenlet;
+        }
+
+        public bool SzobaValtas()
+        {
+
+            if (jelenlet == true)
+            {
+                jelenlet = false;
+                return jelenlet;
+            }
+            else
+            {
+                jelenlet = true;
+                return jelenlet;
+            }
+
+        }
+
+        public bool IttVagyokE()
+        {
+            return jelenlet;
+        }
+
+        public void SzobaNézd()
+        {
+            switch (szobaNeve)
+            {
+                case "szoba":
+                    Console.WriteLine("A szobában vagy, keletre egy ajtó, északra egy szekrény, nyugatra egy ágy");
+                    break;
+
+                case "fürdőszoba":
+                    Console.WriteLine("A fürdőben vagy, itt található egy kád");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Koszones()
+        {
+            Console.WriteLine("Üdvözöllek! \nEz a játék a Szabadulás a szobából! \nA nappaliban állsz és ki kell jutnod! \nA help szót beírva kiírja milyen parancsokat hajthatsz végre! \nJó szórakozást! ");
+        }
+
+        static void Help()
+        {
+            Console.WriteLine("A végrehajtandó cselekedetek: \nmenj (\"menj\" utasítás után égtájat tudsz beírni (észak, kelet, nyugat, dél) pl.: \"menj észak\" )" +
+                                "\nnézd (A nézd utasítás tárggyal használva a tárgyról ad rövid leírást. pl.:\"nézd ágy\"" +
+                                "\nA sima nézd utasítással szobát vizsgálod meg pl.:\"nézd\") " +
+                                "\nvedd fel(A \"vedd fel\" utasítás után tárgyakat írhatsz be és bekerül a táskádba pl.: \"vedd fel kulcs\") " +
+                                "\ntedd le (A \"tedd le\" utasítás után tárgyakat írhatsz be és kikerül a táskádból a szobába pl.: \"tedd le kulcs\")" +
+                                "\nnyisd (A \"nyisd\" utasítás után megfelelő tárgyakat használva kinyilnak pl.:\"nyisd ajtó\", " +
+                                "\n ha magától nem nyitható akkor be kell írni mivel szeretnéd nyitni pl.:\"nyisd ablak kulcs\")" +
+                                "\nhúzd (A \"húzd\" utasítás után megfelelő tárgyat beírva elhúzhatók pl.:\"húzd ágy\")" +
+                                "\ntörd (A \"törd\" utasítás után megfelelő tárgyat beírva, fel tudod törni azt pl.:\"törd szekrény\")" +
+                                "\nleltar (A \"leltar\" utasítás után kilistázza a táskádban lévő dolgokat pl.:\"leltar\")");
+
+        }
+
         static void Main(string[] args)
         {
-            Targy[] targy = new Targy[]
-                {
-                new Targy (0, "szekrény", "szoba", false)
-                new Targy (1, "ajtó", "szoba", false)
-                new Targy (2, "ablak", "szoba", false)
-                new Targy (3, "doboz", "szoba", false)
-                new Targy (4, "kulcs", "szoba", false)
-                new Targy (5, "feszítővas", "fürdőszoba", false)
-                new Targy (6, "kád", "fürdőszoba", false)
-                new Targy (7, "ágy", "szoba", false)
+            #region helységek, tárgyak
 
-                };
+            Helyseg[] helyseg = new Helyseg[]
+            {
+                new Helyseg("szoba",true),
+                new Helyseg("fürdőszoba",false),
+            };
+
+
+            Targy[] targy = new Targy[]
+            {
+                new Targy (0, "szekrény", "szoba", false),
+                new Targy (1, "ajtó", "szoba", false),
+                new Targy (2, "ablak", "szoba", false),
+                new Targy (3, "doboz", "szoba", false),
+                new Targy (4, "kulcs", "szoba", false),
+                new Targy (5, "feszítővas", "fürdőszoba", false),
+                new Targy (6, "kád", "fürdőszoba", false),
+                new Targy (7, "ágy", "szoba", false),
+            };
+
+            #endregion
+
+            Koszones();
+
+
+            Console.ReadLine();
         }
     }
 }
